d58c01e4f02c4db61f438c1db066c4550ee4fe52
Nikolay Martoplyas <kolyan94martoplyas@gmail.com>
2024-09-15 13:18:12 +0300

    added allure

--- src/test/java/ru/netology/DataGenerator.java
+++ src/test/java/ru/netology/DataGenerator.java
@@ -8,30 +8,41 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class DataGenerator {
-    private DataGenerator(){
+    private DataGenerator() {
     }
 
-    public static String generateDate(int shift){
+    public static String generateDate(int shift) {
         return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
-    public static String generateCity(String local){
+
+    public static String generateCity(String local) {
         Faker faker = new Faker(new Locale(local));
         return faker.address().city();
     }
-    public static String generateName(String local){
+
+    public static String generateName(String local) {
         Faker faker = new Faker(new Locale(local));
         return faker.name().fullName();
     }
-    public static String generatePhone(String local){
+
+    public static String generatePhone(String local) {
         Faker faker = new Faker(new Locale(local));
         return faker.phoneNumber().phoneNumber();
     }
+
+    public static String randomPhone(String local) {
+        Faker faker = new Faker(new Locale(local));
+        return faker.numerify("######");
+    }
+
     public static class Registration {
         private Registration() {
         }
-    public static UserInfo generateUser(String local){
+
+        public static UserInfo generateUser(String local) {
             return new UserInfo(generateCity(local), generateName(local), generatePhone(local));
-    }
+        }
+
         @Value
         public static class UserInfo {
             String city;
@@ -41,4 +52,4 @@ public class DataGenerator {
     }
 
 
-    }
+}
--- src/test/java/ru/netology/DeliveryCardTest.java
+++ src/test/java/ru/netology/DeliveryCardTest.java
@@ -2,29 +2,41 @@ package ru.netology;
 
 
 import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class DeliveryCardTest {
+    private final int daysToAddForFirstMeeting = 6;
+    private final String firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         open("http://localhost:9999");
     }
 
     @Test
     @DisplayName("Should successful plan and replan meeting")
-    void shouldSuccessfulPlanAndReplanMeeting(){
+    void shouldSuccessfulPlanAndReplanMeeting() {
 //        var validUser = DataGenerator.Registration.generateUser("ru");
         var daysToAddForFirstMeeting = 5;
         var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
@@ -55,5 +67,21 @@ public class DeliveryCardTest {
         $("[data-test-id='success-notification'] .notification__content")
                 .shouldBe(visible, Duration.ofSeconds(15))
                 .shouldHave(text("Встреча успешно запланирована на " + secondMeetingDate));
+    }
+
+    @Test
+    void invalidPhone() {
+        SelenideElement form = $(".form");
+        form.$("[data-test-id='city'] input").setValue(DataGenerator.generateCity("ru"));
+        form.$("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a", Keys.DELETE);
+        form.$("[data-test-id='date'] input").setValue(firstMeetingDate);
+        form.$("[data-test-id='name'] input").setValue(DataGenerator.generateName("ru"));
+        form.$("[data-test-id='phone'] input").setValue(DataGenerator.randomPhone("en"));
+        form.$("[data-test-id='agreement'] .checkbox__box").click();
+        form.$(".button").click();
+        form.$("[data-test-id='phone'] .input_invalid .input__sub")
+                .shouldHave(exactText("Неверный формат номера мобильного телефона"));
+
+
     }
 }
